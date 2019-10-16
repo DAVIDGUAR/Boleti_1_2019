9c8e8ca75b2044e051d8f4a737e5f4eacf58ad43
david <drodriguezguardiola@danielcastelao.org>
2019-10-16 16:20:15 +0200

    commit

--- src/boletin1_3_2019/boletin1_3_2019.java
+++ src/boletin1_3_2019/boletin1_3_2019.java
@@ -12,44 +12,40 @@ import javax.swing.JOptionPane;
  * @author drodriguezguardiola
  */
 public class boletin1_3_2019 {
-    
-     public static void main(String[] args) {
-         
-         
-         final double conversor=0.91;
-         
-         double euro,dolar,cambio;
-         
-         
-         System.out.println("Elige que quieres cambiar: 1-Euro.\n 2-Dolar");
-         
-         int dato=Integer.parseInt(JOptionPane.showInputDialog("Seleciona una de las opcines anteriores (1/2)"));
-         
-       
-                 
-                 
-          switch (dato){
-              
-              
-              case 1:
-                  
-                  euro=JOptionPane.showInputDialog("Introduce los euros a cambiar")
-                  
-                  cambio=euro*coversor;
-              
-              
-              break;
-          }      
-         
-         
-         double cambio_euro=conversor*dolar;
-         
-        double cambio_dolar=conversor*euro;
-         
-         
-         
-     }
-    
-    
-    
+
+    public static void main(String[] args) {
+
+        final double conversor = 1.10;//no se podria declarar como final ya que es un valor en constante cambio.
+
+        double euro, dolar, cambio;
+
+        System.out.println("Elige que quieres cambiar:\n 1-Euro.\n 2-Dolar");
+
+        int dato = Integer.parseInt(JOptionPane.showInputDialog("Seleciona una de las opcines anteriores (1/2)"));
+
+        switch (dato) {
+
+            case 1:
+
+                euro = Integer.parseInt(JOptionPane.showInputDialog("Introduce los euros a cambiar"));
+
+                cambio = euro * conversor;
+
+                System.out.println("Por " + euro + " €, tendras " + cambio + " $. ");
+
+                break;
+
+            case 2:
+
+                dolar = Integer.parseInt(JOptionPane.showInputDialog("Introduce los dolar a cambiar"));
+
+                cambio = dolar / conversor;
+
+                System.out.println("Por " + dolar + " €, tendras " + cambio + " $. ");
+
+                break;
+
+        }
+    }
+
 }
