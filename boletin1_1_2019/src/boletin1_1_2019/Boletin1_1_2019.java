75317645649c4fb614b797591216bc61dbc480e4
david <drodriguezguardiola@danielcastelao.org>
2019-10-16 16:20:57 +0200

    commit

---
+++ boletin1_1_2019/src/boletin1_1_2019/Boletin1_1_2019.java
@@ -0,0 +1,31 @@
+package boletin1_1_2019;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author drodriguezguardiola
+ */
+public class Boletin1_1_2019 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        
+        
+        float area,base=4,altura=3;
+        
+        area=base*altura;
+        
+        System.out.println("El area del rectangulo es : " + area);
+        
+    }
+    
+}
