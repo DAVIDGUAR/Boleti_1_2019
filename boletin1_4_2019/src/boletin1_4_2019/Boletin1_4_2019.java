515c28e06673d0550fbf0f2d5b3c838da2a7567b
david <drodriguezguardiola@danielcastelao.org>
2019-10-16 17:04:49 +0200

    commit

--- boletin1_4_2019/src/boletin1_4_2019/Boletin1_4_2019.java
+++ boletin1_4_2019/src/boletin1_4_2019/Boletin1_4_2019.java
@@ -5,21 +5,40 @@
  */
 package boletin1_4_2019;
 
+import java.util.Scanner;
+
 /**
  *
  * @author david
  */
 public class Boletin1_4_2019 {
-    
-    
-    
+
     public static void main(String[] args) {
+
+        double num1, num2, suma, resta, producto, coeficiente;
+
+        System.out.println("Introduce el primer numero");
         
+        Scanner numa = new Scanner(System.in);
+
+        num1 = numa.nextDouble();
         
+        System.out.println("Introduce el segundo numero");
         
+        Scanner numb = new Scanner(System.in);
+
+        num2 = numb.nextDouble();
+
+        suma = num1 + num2;
+
+        resta = num1 - num2;
+
+        producto = num1 * num2;
+
+        coeficiente = num1 / num2;
         
-        
-        
+        System.out.println("Suma= "+suma+ "\nResta = " + resta + "\nMultiplicacion5 = " +producto+ "\nDivision = " +coeficiente);
+
     }
-    
+
 }
