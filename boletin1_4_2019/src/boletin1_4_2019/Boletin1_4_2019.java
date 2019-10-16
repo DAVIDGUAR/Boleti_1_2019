fe362027b3196e2e67a2ecb74b17af7893835543
david <drodriguezguardiola@danielcastelao.org>
2019-10-16 16:26:43 +0200

    commit

---
+++ boletin1_4_2019/src/boletin1_4_2019/Boletin1_4_2019.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin1_4_2019;
+
+/**
+ *
+ * @author david
+ */
+public class Boletin1_4_2019 {
+    
+    
+    
+    public static void main(String[] args) {
+        
+        
+        
+        
+        
+        
+    }
+    
+}
